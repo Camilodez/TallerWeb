e4a997ba921c4ce6d58ed8ada57e6b3a841d3609
Michael Gonzalez <michaeljoelgd@gmail.com>
2024-03-28 14:38:49 -0500

    Busca el usuario e imprime datos simples

--- src/app/barra/barra-busqueda/barra-busqueda.component.ts
+++ src/app/barra/barra-busqueda/barra-busqueda.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-barra-busqueda',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./barra-busqueda.component.css']
 })
 export class BarraBusquedaComponent {
+
+  ROOT_URL = "https://dummyjson.com";
+
+  txtuser: string = "";
+
+  constructor(private http: HttpClient) { }
+
+  user$: Observable<any> = new Observable();
+
+  usuario: User | null = null;
+    
+  
+
+  buscarUsuario() {
+    this.user$ = this.http.get(`${this.ROOT_URL}/users/filter?key=username&value=${this.txtuser}`);
+    
+    this.user$.subscribe( data => {
+      const userData = data.users[0]
+      this.usuario = userData;
+    })
+  }
 
 }
