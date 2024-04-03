b7fbb2de0c480d989c86481a27d74c8fb516bb31
Camilodez <hernandezz_camilo@javeriana.edu.co>
2024-04-03 11:34:55 -0500

    header hecho con disenio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable, mergeMap, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { User } from './models/User';
 import { Post } from './models/Post';
 
@@ -11,28 +11,34 @@ import { Post } from './models/Post';
 })
 export class AppComponent {
   title = 'TallerWeb';
-
   ROOT_URL = "https://dummyjson.com";
-
   txtuser: string = "";
-
-  constructor(private http: HttpClient) { }
-
   user$: Observable<any> = new Observable();
-
   usuario: User | null = null;
-
   posts: Post | null = null;
+  isInputExpanded: boolean = false; // Agregado para controlar la expansión del input
+
+  constructor(private http: HttpClient) {}
 
   buscarUsuario() {
+    if (!this.isInputExpanded) {
+      // Si el input no está expandido, expandirlo y no realizar la búsqueda
+      this.isInputExpanded = true;
+      return;
+    }
 
     this.user$ = this.http.get(`${this.ROOT_URL}/users/filter?key=username&value=${this.txtuser}`);
-    this.user$.subscribe( data => {
+    this.user$.subscribe(data => {
       this.usuario = data.users[0];
-      console.log(this.usuario)
-    })
-
+      console.log(this.usuario);
+    });
   }
 
-}
+  toggleInput() {
+    this.isInputExpanded = !this.isInputExpanded;
+  }
 
+  expandInput() {
+    this.isInputExpanded = true;
+  }
+}
