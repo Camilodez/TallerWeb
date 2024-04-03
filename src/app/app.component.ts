13a8447572bf68f4ce9e1fbee2c1c9bab7fa8fa1
Michael Gonzalez <michaeljoelgd@gmail.com>
2024-04-02 21:43:30 -0500

    Ya funciona todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,50 +19,20 @@ export class AppComponent {
   constructor(private http: HttpClient) { }
 
   user$: Observable<any> = new Observable();
-  post$: Observable<any> = new Observable();
 
   usuario: User | null = null;
 
   posts: Post | null = null;
 
   buscarUsuario() {
+
     this.user$ = this.http.get(`${this.ROOT_URL}/users/filter?key=username&value=${this.txtuser}`);
     this.user$.subscribe( data => {
       this.usuario = data.users[0];
       console.log(this.usuario)
     })
 
-    /*this.http.get(`${this.ROOT_URL}/users/filter?key=username&value=${this.txtuser}`).subscribe((data: any) => {
-        if(data) {
-          this.usuario = data.users[0];
-          console.log(this.usuario)
-        }else{
-          this.usuario = null
-          console.log("No")
-        }
-    })*/
-
-    //this.obtenerPost();
-  }
-
-  obtenerPost() {
-
-    /*this.http.get<User>(`${this.ROOT_URL}/users/filter?key=username&value=${this.txtuser}`).pipe(
-      mergeMap((userInfo : any ) => {
-        if(userInfo != null) {
-          this.usuario = userInfo[0];
-          return this.http.get<Post>(this.ROOT_URL + '/post/user/' + this.usuario!.id);
-        }
-        else{
-          this.usuario = null;
-          return of(0);
-        }
-      })
-    ).subscribe((postInfo: any) => {
-      this.posts = postInfo[0];
-    })
-
-  }*/
   }
 
 }
+
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../models/Post';
+import { HttpClient } from '@angular/common/http';
+import axios, { AxiosResponse } from 'axios';
+import { Comentario } from '../models/Comentario';
 
 @Component({
   selector: 'app-post',
@@ -7,5 +10,49 @@ import { Post } from '../models/Post';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-  @Input () raiz: Post | null = null;
+  @Input () id?: number ;
+
+  ROOT_URL = "https://dummyjson.com"
+
+  constructor(private http: HttpClient) { }
+
+  publicaciones: Post[] = []
+
+  comentarios: Comentario[] = []
+
+  ngOnChanges() {
+    if(this.id) {
+      setTimeout(() => this.buscarPosts(), 50);
+
+    }
+  }
+
+  async buscarPosts() {
+    
+    try {
+      const response: AxiosResponse = await axios.get(`${this.ROOT_URL}/posts/user/${this.id}`)
+      console.log(response)
+      this.publicaciones = response.data.posts
+
+      this.publicaciones.forEach((post) => {
+        this.buscarComentarios(post.id)
+      })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async buscarComentarios(idcomment: number) {
+
+    this.comentarios = []
+    try {
+    const response = await axios.get(`${this.ROOT_URL}/comments/post/${idcomment}`)
+    console.log(response)
+
+    this.comentarios = response.data.comments
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
